94fa845669a2e8751ac8614df34582d22db16bb0
LeoDumas <leo.ndumas980@gmail.com>
2026-04-24 12:21:05 +0200

    ADD: RTL Full_adder

--- TP1_additionneur/full_adder_4b.vhd
+++ TP1_additionneur/full_adder_4b.vhd
@@ -9,14 +9,24 @@ entity full_adder_4b is
 
         A: in std_logic_vector(3 downto 0); -- Première opérande (4bits)
         B: in std_logic_vector(3 downto 0); -- Deuxième opérande (4bits)
+        Cin: in std_logic; -- Retenue ) l'entrée
 
-        S: out std_logic; -- Somme de A+B
-        C: out std_logic  -- Retenue de A+B 
+        S: out std_logic; -- Somme de A+B + Cin
+        Cout: out std_logic  -- Retenue de la sortie
 	);
 end full_adder_4b;
 
 -- DESCRIPTION COMPORTEMENTALE DU HALF ADDER
 architecture behavioral of full_adder_4b is
+    signal A1: std_logic;
+    signal A2: std_logic;
+    signal A3: std_logic;
+    signal A4: std_logic;
+
+    signal B1: std_logic;
+    signal B2: std_logic;
+    signal B3: std_logic;
+    signal B4: std_logic;
 begin
     -- Ecrire ici les instructions cocurrentes décrivant le comportement de l'entity
 	-- ex. X <= not(A);
