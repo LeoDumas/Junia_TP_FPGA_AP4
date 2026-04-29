5405b7616a8683e597418c20011f63bfc94410da
Arthaud <arthaudp33@gmail.com>
2026-04-29 11:22:20 +0200

    :bug: Fix quotes

--- TP1_additionneur/tb_full_adder_4b.vhd
+++ TP1_additionneur/tb_full_adder_4b.vhd
@@ -21,90 +21,90 @@ begin
 
 	add_2_bits_without_Cin_without_Cout : process begin
 		-- 1+1+0
-		A <= '0001';
-		B <= '0001';
-		Cin <= '0';
+		A <= "0001";
+		B <= "0001";
+		Cin <= "0";
 		
 		wait for 10ns;
 
-		assert S = '0010' report "Sum error in full_adder_4b (1+1+0)" severity error;
-		assert Cout = '0' report "Carry error in full_adder_4b (1+1+0)" severity error;
+		assert S = "0010" report "Sum error in full_adder_4b (1+1+0)" severity error;
+		assert Cout = "0" report "Carry error in full_adder_4b (1+1+0)" severity error;
 
 
 		-- 5+5+0
-		A <= '0101';
-		B <= '0101';
-		Cin <= '0';
+		A <= "0101";
+		B <= "0101";
+		Cin <= "0";
 		
 		wait for 10ns;
 
-		assert S = '1010' report "Sum error in full_adder_4b (5+5+0)" severity error;
-		assert Cout = '0' report "Carry error in full_adder_4b (5+5+0)" severity error;
+		assert S = "1010" report "Sum error in full_adder_4b (5+5+0)" severity error;
+		assert Cout = "0" report "Carry error in full_adder_4b (5+5+0)" severity error;
 	end process;
 
 	add_2_bits_without_Cin_with_Cout : process begin
 		-- 9+9+0
-		A <= '1001';
-		B <= '1001';
-		Cin <= '0';
+		A <= "1001";
+		B <= "1001";
+		Cin <= "0";
 		
 		wait for 10ns;
 
-		assert S = '0010' report "Sum error in full_adder_4b (9+9+0)" severity error;
-		assert Cout = '1' report "Carry error in full_adder_4b (9+9+0)" severity error;
+		assert S = "0010" report "Sum error in full_adder_4b (9+9+0)" severity error;
+		assert Cout = "1" report "Carry error in full_adder_4b (9+9+0)" severity error;
 
 		-- 15+15+0
-		A <= '1111';
-		B <= '1111';
-		Cin <= '0';
+		A <= "1111";
+		B <= "1111";
+		Cin <= "0";
 		
 		wait for 10ns;
 
-		assert S = '1110' report "Sum error in full_adder_4b (15+15+0)" severity error;
-		assert Cout = '1' report "Carry error in full_adder_4b (15+15+0)" severity error;
+		assert S = "1110" report "Sum error in full_adder_4b (15+15+0)" severity error;
+		assert Cout = "1" report "Carry error in full_adder_4b (15+15+0)" severity error;
 	end process;
 
 	add_2_bits_with_Cin_without_Cout : process begin
 		-- 1+1+1
-		A <= '0001';
-		B <= '0001';
-		Cin <= '1';
+		A <= "0001";
+		B <= "0001";
+		Cin <= "1";
 		
 		wait for 10ns;
 
-		assert S = '0011' report "Sum error in full_adder_4b (1+1+1)" severity error;
-		assert Cout = '0' report "Carry error in full_adder_4b (1+1+1)" severity error;
+		assert S = "0011" report "Sum error in full_adder_4b (1+1+1)" severity error;
+		assert Cout = "0" report "Carry error in full_adder_4b (1+1+1)" severity error;
 
 		-- 3+3+1
-		A <= '0011';
-		B <= '0011';
-		Cin <= '1';
+		A <= "0011";
+		B <= "0011";
+		Cin <= "1";
 		
 		wait for 10ns;
 
-		assert S = '0111' report "Sum error in full_adder_4b (3+3+1)" severity error;
-		assert Cout = '0' report "Carry error in full_adder_4b (3+3+1)" severity error;
+		assert S = "0111" report "Sum error in full_adder_4b (3+3+1)" severity error;
+		assert Cout = "0" report "Carry error in full_adder_4b (3+3+1)" severity error;
 	end process;
 
 	add_2_bits_with_Cin_with_Cout : process begin
 		-- 15+15+1
-		A <= '1111';
-		B <= '1111';
-		Cin <= '1';
+		A <= "1111";
+		B <= "1111";
+		Cin <= "1";
 		
 		wait for 10ns;
 
-		assert S = '1111' report "Sum error in full_adder_4b (15+15+1)" severity error;
-		assert Cout = '1' report "Carry error in full_adder_4b (15+15+1)" severity error;
+		assert S = "1111" report "Sum error in full_adder_4b (15+15+1)" severity error;
+		assert Cout = "1" report "Carry error in full_adder_4b (15+15+1)" severity error;
 
 		-- 8+8+1
-		A <= '1000';
-		B <= '1000';
-		Cin <= '1';
+		A <= "1000";
+		B <= "1000";
+		Cin <= "1";
 		
 		wait for 10ns;
 
-		assert S = '0001' report "Sum error in full_adder_4b (8+8+1)" severity error;
-		assert Cout = '1' report "Carry error in full_adder_4b (8+8+1)" severity error;
+		assert S = "0001" report "Sum error in full_adder_4b (8+8+1)" severity error;
+		assert Cout = "1" report "Carry error in full_adder_4b (8+8+1)" severity error;
 	end process;
 end tb ;
