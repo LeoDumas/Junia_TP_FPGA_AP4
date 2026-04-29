40222374e667edb8afab73239a7c964dba7b3213
LeoDumas <leo.ndumas980@gmail.com>
2026-04-29 09:31:09 +0200

    ADD: full_adder_4b

--- TP1_additionneur/full_adder_4b.vhd
+++ TP1_additionneur/full_adder_4b.vhd
@@ -2,35 +2,57 @@ library ieee ;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- DESCRIPTION DES ENTREES/SORTIES DU HALF ADDER
+-- DESCRIPTION DES ENTREES/SORTIES DU FULL ADDER 4B
 
 entity full_adder_4b is
 	port (
-
-        A: in std_logic_vector(3 downto 0); -- Première opérande (4bits)
-        B: in std_logic_vector(3 downto 0); -- Deuxième opérande (4bits)
+        -- Entrées
+        A, B: in std_logic_vector(3 downto 0); -- Première et deuxième opérande (4bits)
         Cin: in std_logic; -- Retenue ) l'entrée
 
-        S: out std_logic; -- Somme de A+B + Cin
+        -- Sorties
+        S: out std_logic_vector(3 downto 0); -- Somme de A+B + Cin
         Cout: out std_logic  -- Retenue de la sortie
 	);
 end full_adder_4b;
 
--- DESCRIPTION COMPORTEMENTALE DU HALF ADDER
+-- DESCRIPTION COMPORTEMENTALE DU FULL ADDER 4B
 architecture behavioral of full_adder_4b is
-    signal A1: std_logic;
-    signal A2: std_logic;
-    signal A3: std_logic;
-    signal A4: std_logic;
-
-    signal B1: std_logic;
-    signal B2: std_logic;
-    signal B3: std_logic;
-    signal B4: std_logic;
+    signal C: std_logic_vector(4 downto 0);
 begin
-    -- Ecrire ici les instructions cocurrentes décrivant le comportement de l'entity
-	-- ex. X <= not(A);
-	-- ex. S <= not(X);
-    S <= A xor B; 
-    C <= A and B;
+    C(4) <= Cin;
+    Cout <= C(0);
+
+    instance_full_adder_1 : entity work.full_adder port map(
+        A => A(3),
+        B => B(3),
+        Cin => C(4),
+        S => S(3),
+        Cout => C(3)
+    );
+
+    instance_full_adder_2 : entity work.full_adder port map(
+        A => A(2),
+        B => B(2),
+        Cin => C(3),
+        S => S(2),
+        Cout => C(2)
+    );
+
+    instance_full_adder_3 : entity work.full_adder port map(
+        A => A(1),
+        B => B(1),
+        Cin => C(2),
+        S => S(1),
+        Cout => C(1)
+    );
+
+    instance_full_adder_4 : entity work.full_adder port map(
+        A => A(0),
+        B => B(0),
+        Cin => C(1),
+        S => S(0),
+        Cout => C(0)
+    );
+
 end behavioral;
