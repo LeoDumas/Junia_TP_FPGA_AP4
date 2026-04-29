b59260a0a04fdeb4427a225a81238fa3e4c5871c
LeoDumas <leo.ndumas980@gmail.com>
2026-04-29 09:38:19 +0200

    FIX: Inverted bits

--- TP1_additionneur/full_adder_4b.vhd
+++ TP1_additionneur/full_adder_4b.vhd
@@ -20,39 +20,39 @@ end full_adder_4b;
 architecture behavioral of full_adder_4b is
     signal C: std_logic_vector(4 downto 0);
 begin
-    C(4) <= Cin;
-    Cout <= C(0);
+    C(0) <= Cin;
+    Cout <= C(4);
 
     instance_full_adder_1 : entity work.full_adder port map(
-        A => A(3),
-        B => B(3),
-        Cin => C(4),
-        S => S(3),
-        Cout => C(3)
+        A => A(0),
+        B => B(0),
+        Cin => C(0),
+        S => S(0),
+        Cout => C(1)
     );
 
     instance_full_adder_2 : entity work.full_adder port map(
-        A => A(2),
-        B => B(2),
-        Cin => C(3),
-        S => S(2),
+        A => A(1),
+        B => B(1),
+        Cin => C(1),
+        S => S(1),
         Cout => C(2)
     );
 
     instance_full_adder_3 : entity work.full_adder port map(
-        A => A(1),
-        B => B(1),
+        A => A(2),
+        B => B(2),
         Cin => C(2),
-        S => S(1),
-        Cout => C(1)
+        S => S(2),
+        Cout => C(3)
     );
 
     instance_full_adder_4 : entity work.full_adder port map(
-        A => A(0),
-        B => B(0),
-        Cin => C(1),
-        S => S(0),
-        Cout => C(0)
+        A => A(3),
+        B => B(3),
+        Cin => C(3),
+        S => S(3),
+        Cout => C(4)
     );
 
 end behavioral;
