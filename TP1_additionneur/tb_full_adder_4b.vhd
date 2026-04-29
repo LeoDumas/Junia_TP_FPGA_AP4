d904bf5f9fd2bce2f332c5a249cd2a7c54941cee
Arthaud <arthaudp33@gmail.com>
2026-04-29 11:07:14 +0200

    ✅ Test bench of full adder 4b

--- TP1_additionneur/tb_full_adder_4b.vhd
+++ TP1_additionneur/tb_full_adder_4b.vhd
@@ -2,33 +2,109 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- DECLARATION D'UNE ENTITE
 entity tb_full_adder_4b is
 end tb_full_adder_4b;
 
 architecture tb of tb_full_adder_4b is
-    -- Déclaration des signaux de test
-	-- ex. signal A, S: std_logic;
-
+	signal A, B, S : std_logic_vector(3 downto 0);
+	signal Cin, Cout : std_logic;
 begin
     -- Instanciation de l'entité testée, récupérée dans la librairie work
     -- On appelle cette instance UUT (Unit Under Test)
-	UUT : entity work.entity_name port map (
-        -- ex. A => A,
-        -- ex. S => S
+	UUT : entity work.full_adder_4b port map (
+        A => A,
+        B => B,
+		Cin => Cin,
+		S => S,
+		Cout => Cout
 	);
 
-	-- Description des stimuli
-	-- A placer dans des *process*, car ce sont des instructions séquentielles
-	-- (Plusieurs process concurrents peuvent être décrits)
-	stimuli1 : process
-		begin
-		-- ex. A <= '0';
-		-- ex. wait for 10ns; -- Délai avant le prochain stimulus
-		-- ex. A <= '1';
-		-- ex. wait for 10ns; -- Délai avant le prochain stimulus
+	add_2_bits_without_Cin_without_Cout : process begin
+		-- 1+1+0
+		A <= '0001';
+		B <= '0001';
+		Cin <= '0';
+		
+		wait for 10ns;
 
-		wait; -- Wait indefinitely
+		assert S = '0010' report "Sum error in full_adder_4b (1+1+0)" severity error;
+		assert Cout = '0' report "Carry error in full_adder_4b (1+1+0)" severity error;
 
+
+		-- 5+5+0
+		A <= '0101';
+		B <= '0101';
+		Cin <= '0';
+		
+		wait for 10ns;
+
+		assert S = '1010' report "Sum error in full_adder_4b (5+5+0)" severity error;
+		assert Cout = '0' report "Carry error in full_adder_4b (5+5+0)" severity error;
+	end process;
+
+	add_2_bits_without_Cin_with_Cout : process begin
+		-- 9+9+0
+		A <= '1001';
+		B <= '1001';
+		Cin <= '0';
+		
+		wait for 10ns;
+
+		assert S = '0010' report "Sum error in full_adder_4b (9+9+0)" severity error;
+		assert Cout = '1' report "Carry error in full_adder_4b (9+9+0)" severity error;
+
+		-- 15+15+0
+		A <= '1111';
+		B <= '1111';
+		Cin <= '0';
+		
+		wait for 10ns;
+
+		assert S = '1110' report "Sum error in full_adder_4b (15+15+0)" severity error;
+		assert Cout = '1' report "Carry error in full_adder_4b (15+15+0)" severity error;
+	end process;
+
+	add_2_bits_with_Cin_without_Cout : process begin
+		-- 1+1+1
+		A <= '0001';
+		B <= '0001';
+		Cin <= '1';
+		
+		wait for 10ns;
+
+		assert S = '0011' report "Sum error in full_adder_4b (1+1+1)" severity error;
+		assert Cout = '0' report "Carry error in full_adder_4b (1+1+1)" severity error;
+
+		-- 3+3+1
+		A <= '0011';
+		B <= '0011';
+		Cin <= '1';
+		
+		wait for 10ns;
+
+		assert S = '0111' report "Sum error in full_adder_4b (3+3+1)" severity error;
+		assert Cout = '0' report "Carry error in full_adder_4b (3+3+1)" severity error;
+	end process;
+
+	add_2_bits_with_Cin_with_Cout : process begin
+		-- 15+15+1
+		A <= '1111';
+		B <= '1111';
+		Cin <= '1';
+		
+		wait for 10ns;
+
+		assert S = '1111' report "Sum error in full_adder_4b (15+15+1)" severity error;
+		assert Cout = '1' report "Carry error in full_adder_4b (15+15+1)" severity error;
+
+		-- 8+8+1
+		A <= '1000';
+		B <= '1000';
+		Cin <= '1';
+		
+		wait for 10ns;
+
+		assert S = '0001' report "Sum error in full_adder_4b (8+8+1)" severity error;
+		assert Cout = '1' report "Carry error in full_adder_4b (8+8+1)" severity error;
 	end process;
 end tb ;
