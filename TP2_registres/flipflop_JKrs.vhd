4f2ab79ed5d49d444a2f05220d3f1d25f1c8bb25
Arthaud <arthaudp33@gmail.com>
2026-05-04 14:51:39 +0200

    JK rs without var

--- TP2_registres/flipflop_JKrs.vhd
+++ TP2_registres/flipflop_JKrs.vhd
@@ -29,36 +29,25 @@ entity flipflop_JK is
 end flipflop_JK;
 
 architecture behavioral of flipflop_JK is
+    signal Qint : std_logic := '0'; -- état interne
 begin
     process(CLK, RSTn)
-        -- Variable pour stocker l'état et permettre le basculement
-        variable TMP: std_logic;
     begin
         if (RSTn = '0') then
             -- Reset asynchrone : agit immédiatement, indépendamment de l'horloge
-            TMP := '0'; -- Réinitialiser la mémoire interne
-            Q   <= '0';
-            Qn  <= '1';
-            
+            TMQint  <= '0';
+            Q       <= '0';
+            Qn      <= '1';
+        
         elsif rising_edge(CLK) then 
-            -- Front montant de l'horloge
-            -- Pas de changement (verrou)
-            if (J='0' and K='0') then
-                TMP := TMP;
-            -- Bascule (Q s'inverse)
-            elsif (J='1' and K='1') then 
-                TMP := not TMP;
-            -- Réinitialiser (Q devient 0)
-            elsif (J='0' and K='1') then
-                TMP := '0';
-            -- Set (Q devient 1)
-            else
-                TMP := '1';
-            end if;
-            
+            Qint <= Qint        when (J='0' and K='0') else -- Pas de changement (verrou)
+                    not Qint    when (J='1' and K='1') else -- Bascule (Q devient 1 si était 0, inversement)
+                    '0'         when (J='0' and K='1') else -- Réinitialiser (Q devient 0)
+                    '1'         ;                           -- Set (Q devient 1)
+
             -- Mise à jour des sorties de manière synchrone
-            Q  <= TMP;
-            Qn <= not TMP;
+            Q <= Qint;
+            Qn <= not Qint;
         end if;
     end process;
 end behavioral;
