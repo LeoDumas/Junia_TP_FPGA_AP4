b45f9b8b71a68964830de747b971ce3ffead2410
Arthaud <arthaudp33@gmail.com>
2026-05-04 14:45:07 +0200

    :recycle: JK Without var

--- TP2_registres/flipflop_JK.vhd
+++ TP2_registres/flipflop_JK.vhd
@@ -20,6 +20,7 @@ entity flipflop_JK is
         J : in std_logic; -- Entrée J
         K : in std_logic; -- Entrée K
         CLK : in std_logic; -- Horloge (sensibilité au front montant)
+
         -- Sorties
         Q : out std_logic; -- Sortie de la bascule
         Qn : out std_logic -- Sortie complémentée de la bascule
@@ -27,29 +28,18 @@ entity flipflop_JK is
 end flipflop_JK;
 
 architecture behavioral of flipflop_JK is
+    signal Qint : std_logic := '0'; -- état interne
 begin
     process(CLK)
-    -- Stocker l'état précédent pour faire les flip
-    variable TMP: std_logic;
     begin
-        -- Front montant
-        if (CLK'event and CLK = '1') then 
-            -- Pas de changement (verrou)
-            if(J='0' and K='0')then
-                TMP:=TMP;
-            -- Bascule (Q devient 1 si était 0, inversement)
-            elsif(J='1' and K='1')then 
-                TMP:= not TMP;
-            -- Réinitialiser (Q devient 0)
-            elsif(J='0' and K='1')then
-                TMP:='0';
-            -- Set (Q devient 1)
-            else
-                TMP:='1';
-            end if;
+        if rising_edge(CLK) then 
+            Qint <= Qint        when (J='0' and K='0') else -- Pas de changement (verrou)
+                    not Qint    when (J='1' and K='1') else -- Bascule (Q devient 1 si était 0, inversement)
+                    '0'         when (J='0' and K='1') else -- Réinitialiser (Q devient 0)
+                    '1'         ;                           -- Set (Q devient 1)
         end if;
 
-        Q<=TMP;
-        Qn<=not TMP;
+        Q <= Qint;
+        Qn <= not Qint;
     end process;
 end behavioral;
