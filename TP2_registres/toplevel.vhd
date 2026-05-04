43920ce1f14eff88e9d0e4cd1655781cee82c698
Arthaud <arthaudp33@gmail.com>
2026-05-04 16:57:47 +0200

    :sparkles: TP2 1.a.3 toplevel

---
+++ TP2_registres/toplevel.vhd
@@ -0,0 +1,31 @@
+library ieee ;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity toplevel is
+	port (
+		-- Entrées
+		SW : in std_logic_vector(9 downto 0);
+		-- Sorties
+		LEDG : out std_logic_vector(7 downto 0);
+		KEY : out std_logic_vector(3 downto 0);
+	);
+end toplevel;
+
+architecture behavioral of toplevel is
+	signal S, Cout : std_logic_vector(3 downto 0);
+begin
+    instance_shift_register_universal8 : entity work.shift_register_universal8 port map (
+        -- Entrées
+        SSR => SW(9),
+        SSL => SW(8),
+        Pi => 0,
+        SEL => SW(2,0),
+        CLK => not KEY(0),
+        SETn => KEY(2),
+        RSTn => KEY(3),
+
+        -- Sorties
+        Qo => LEDG,
+    );
+end behavioral;
