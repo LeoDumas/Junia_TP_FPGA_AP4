25a6c87f1d9ac607a768d03f0dc0f943e370f228
Arthaud <arthaudp33@gmail.com>
2026-05-04 17:18:37 +0200

    :bug: Fix trailing and ports

--- TP2_registres/toplevel.vhd
+++ TP2_registres/toplevel.vhd
@@ -6,9 +6,10 @@ entity toplevel is
 	port (
 		-- Entrées
 		SW : in std_logic_vector(9 downto 0);
+		KEY : in std_logic_vector(3 downto 0);
+		
 		-- Sorties
-		LEDG : out std_logic_vector(7 downto 0);
-		KEY : out std_logic_vector(3 downto 0);
+		LEDG : out std_logic_vector(7 downto 0)
 	);
 end toplevel;
 
@@ -19,13 +20,13 @@ begin
         -- Entrées
         SSR => SW(9),
         SSL => SW(8),
-        Pi => 0,
-        SEL => SW(2,0),
+        Pi => "00000000",
+        SEL => SW(2 downto 0),
         CLK => not KEY(0),
         SETn => KEY(2),
         RSTn => KEY(3),
 
         -- Sorties
-        Qo => LEDG,
+        Qo => LEDG
     );
 end behavioral;
